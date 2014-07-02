7d88c1822a32678e703c2e5bcb97e1aa0d631326
Luke Murphey <Luke@LukeMurphey.net>
2014-07-02 13:17:31 -0500

    Initial version of the web-input app

---
+++ src/README.txt
@@ -0,0 +1,39 @@
+================================================
+Overview
+================================================
+
+This app provides a mechanism for pulling information from web applications.
+
+
+
+================================================
+Configuring Splunk
+================================================
+
+This app exposes a new input type that can be configured in the Splunk Manager. To configure it, create a new "Website Content" input in the Manager under Data inputs.
+
+
+
+================================================
+Getting Support
+================================================
+
+Go to the following website if you need support:
+
+     http://splunk-base.splunk.com/apps/      /answers/
+
+You can access the source-code and get technical details about the app at:
+
+     https://github.com/LukeMurphey/splunk-web-input
+
+
+
+================================================
+Change History
+================================================
+
++---------+------------------------------------------------------------------------------------------------------------------+
+| Version |  Changes                                                                                                         |
++---------+------------------------------------------------------------------------------------------------------------------+
+| 0.5     | Initial release                                                                                                  |
++---------+------------------------------------------------------------------------------------------------------------------+
---
+++ tests/unit.py
@@ -0,0 +1,140 @@
+import unittest
+import sys
+import os
+import time
+import shutil
+import re
+import tempfile
+from StringIO import StringIO
+
+sys.path.append( os.path.join("..", "src", "bin") )
+
+from web_input import URLField, DurationField, SelectorField, WebInput
+from modular_input import Field, FieldValidationException
+
+class TestURLField(unittest.TestCase):
+    
+    def test_url_field_valid(self):
+        url_field = URLField( "test_url_field_valid", "title", "this is a test" )
+        
+        self.assertEqual( url_field.to_python("http://google.com").geturl(), "http://google.com" )
+        self.assertEqual( url_field.to_python("http://google.com/with/path").geturl(), "http://google.com/with/path" )
+        self.assertEqual( url_field.to_python("http://google.com:8080/with/port").geturl(), "http://google.com:8080/with/port" )
+        
+    def test_url_field_invalid(self):
+        url_field = URLField( "test_url_field_invalid", "title", "this is a test" )
+        
+        self.assertRaises( FieldValidationException, lambda: url_field.to_python("hxxp://google.com") )
+        self.assertRaises( FieldValidationException, lambda: url_field.to_python("http://") )
+        self.assertRaises( FieldValidationException, lambda: url_field.to_python("google.com") )
+    
+class TestDurationField(unittest.TestCase):
+    
+    def test_duration_valid(self):
+        duration_field = DurationField( "test_duration_valid", "title", "this is a test" )
+        
+        self.assertEqual( duration_field.to_python("1m"), 60 )
+        self.assertEqual( duration_field.to_python("5m"), 300 )
+        self.assertEqual( duration_field.to_python("5 minute"), 300 )
+        self.assertEqual( duration_field.to_python("5"), 5 )
+        self.assertEqual( duration_field.to_python("5h"), 18000 )
+        self.assertEqual( duration_field.to_python("2d"), 172800 )
+        self.assertEqual( duration_field.to_python("2w"), 86400 * 7 * 2 )
+        
+    def test_url_field_invalid(self):
+        duration_field = DurationField( "test_url_field_invalid", "title", "this is a test" )
+        
+        self.assertRaises( FieldValidationException, lambda: duration_field.to_python("1 treefrog") )
+        self.assertRaises( FieldValidationException, lambda: duration_field.to_python("minute") )   
+    
+class TestWebInput(unittest.TestCase):
+    
+    def setUp(self):
+        self.tmp_dir = tempfile.mkdtemp( prefix="TestWebInput" )
+        #os.makedirs(self.tmp_dir)
+        
+    def tearDown(self):
+        shutil.rmtree( self.tmp_dir )
+        
+    def test_get_file_path(self):
+        self.assertEquals( WebInput.get_file_path( "/Users/lmurphey/Applications/splunk/var/lib/splunk/modinputs/web_input", "web_input://TextCritical.com"), "/Users/lmurphey/Applications/splunk/var/lib/splunk/modinputs/web_input/2c70b6c76574eb4d825bfb194a460558.json")
+        
+    def test_input_timeout(self):
+        url_field = URLField( "test_input_timeout", "title", "this is a test" )
+        selector_field = SelectorField( "test_web_input_css", "title", "this is a test" )
+        result = WebInput.scrape_page( url_field.to_python("https://192.168.30.23/"), selector_field.to_python("div"), timeout=3 )
+        
+        self.assertEquals(result['timed_out'], True)
+        
+    def test_save_checkpoint(self):
+        WebInput.save_checkpoint(self.tmp_dir, "web_input://TextCritical.com", 100)
+        self.assertEquals( WebInput.last_ran(self.tmp_dir, "web_input://TextCritical.com"), 100)
+        
+    def test_is_expired(self):
+        self.assertFalse( WebInput.is_expired(time.time(), 30) )
+        self.assertTrue( WebInput.is_expired(time.time() - 31, 30) )
+        
+    def get_test_dir(self):
+        return os.path.dirname(os.path.abspath(__file__))
+        
+    def test_needs_another_run(self):
+        
+        # Test case where file does not exist
+        self.assertTrue( WebInput.needs_another_run( "/Users/lmurphey/Applications/splunk/var/lib/splunk/modinputs/web_input", "web_input://DoesNotExist", 60 ) )
+        
+        # Test an interval right at the earlier edge
+        self.assertFalse( WebInput.needs_another_run( os.path.join( self.get_test_dir(), "configs" ), "web_input://TextCritical.com", 60, 1365486765 ) )
+        
+        # Test an interval at the later edge
+        self.assertFalse( WebInput.needs_another_run( os.path.join( self.get_test_dir(), "configs" ), "web_input://TextCritical.com", 10, 1365486775 ) )
+        
+        # Test interval beyond later edge
+        self.assertTrue( WebInput.needs_another_run( os.path.join( self.get_test_dir(), "configs" ), "web_input://TextCritical.com", 10, 1365486776 ) )
+    
+    def test_scrape_page(self):
+        web_input = WebInput(timeout=3)
+        
+        url_field = URLField( "test_web_input", "title", "this is a test" )
+        selector_field = SelectorField( "test_web_input_css", "title", "this is a test" )
+        result = WebInput.scrape_page( url_field.to_python("http://textcritical.net/"), selector_field.to_python(".hero-unit.main_background") )
+        self.assertEqual(result['response_code'], 200)
+        self.assertEqual(len(result['matches']), 1)
+        
+    def test_scrape_page_mv(self):
+        web_input = WebInput(timeout=3)
+        
+        url_field = URLField( "test_web_input", "title", "this is a test" )
+        selector_field = SelectorField( "test_web_input_css", "title", "this is a test" )
+        result = WebInput.scrape_page( url_field.to_python("http://textcritical.net/"), selector_field.to_python("h2") )
+        self.assertEqual(result['response_code'], 200)
+        self.assertEqual(len(result['matches']), 3)
+        
+        out = StringIO()
+        web_input.output_event(result, stanza="web_input://textcritical_net", index="main", source="test_web_input", sourcetype="sourcetype", out=out)
+        print out.getvalue()
+        self.assertEquals( len(re.findall("matches=", out.getvalue())), 3)
+        
+    def test_scrape_unavailable_page(self):
+        web_input = WebInput(timeout=3)
+        
+        url_field = URLField( "test_web_input", "title", "this is a test" )
+        selector_field = SelectorField( "test_web_input_css", "title", "this is a test" )
+        result = WebInput.scrape_page( url_field.to_python("http://192.168.30.23/"), selector_field.to_python(".hero-unit.main_background"), timeout=3 )
+        
+        self.assertEqual(result['timed_out'], True)
+    
+    def test_unparsable(self):
+        web_input = WebInput(timeout=3)
+        
+        url_field = URLField( "test_web_input", "title", "this is a test" )
+        selector_field = SelectorField( "test_web_input_css", "title", "this is a test" )
+        result = WebInput.scrape_page( url_field.to_python("http://textcritical.net/media/images/link_external.png"), selector_field.to_python(".hero-unit .main_background"), timeout=3 )
+        self.assertEqual(result['matches'], [])
+        
+        
+if __name__ == "__main__":
+    loader = unittest.TestLoader()
+    suites = []
+    suites.append(loader.loadTestsFromTestCase(TestWebInput))
+    
+    unittest.TextTestRunner(verbosity=2).run(unittest.TestSuite(suites))
