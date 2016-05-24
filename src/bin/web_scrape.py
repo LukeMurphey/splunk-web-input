177adde9523334357e079cc0c0c6bbce2ae4b356
Luke Murphey <Luke@LukeMurphey.net>
2016-05-24 01:22:58 -0500

    Adding support for command variables to the search command

--- src/bin/web_scrape.py
+++ src/bin/web_scrape.py
@@ -5,21 +5,31 @@ from splunk.util import normalizeBoolean
 
 class WebScraper(SearchCommand):
     
-    def __init__(self, url, selector, username=None, password=None, timeout=30, name_attributes=[], output_matches_as_mv=True, output_matches_as_separate_fields=False, use_element_name=False, page_limit=1, depth_limit=50, url_filter=None):
+    def __init__(self, url, selector, username=None, password=None, timeout=30, name_attributes=[], output_matches_as_mv=True, output_matches_as_separate_fields=False, use_element_name=False, page_limit=1, depth_limit=50, url_filter=None, text_separator=" ", include_raw_content=False):
         
         # Save the parameters
-        self.url = url
-        self.selector = selector
-        self.username = username
-        self.password = password
-        self.timeout = timeout
-        self.name_attributes = name_attributes
-        self.output_matches_as_mv = normalizeBoolean(output_matches_as_mv)
-        self.output_matches_as_separate_fields = normalizeBoolean(output_matches_as_separate_fields)
-        self.use_element_name = normalizeBoolean(use_element_name)
-        self.page_limit = int(page_limit)
-        self.depth_limit = int(depth_limit)
-        self.url_filter = url_filter
+        self.params = {
+                       "url": url,
+                       "selector": selector,
+                       "username": username,
+                       "password": password,
+                       "timeout": timeout,
+                       "name_attributes": name_attributes,
+                       "output_matches_as_mv": normalizeBoolean(output_matches_as_mv),
+                       "output_matches_as_separate_fields": normalizeBoolean(output_matches_as_separate_fields),
+                       "include_empty_matches": False,
+                       "proxy_type": "http",
+                       "proxy_server": None,
+                       "proxy_port": None,
+                       "proxy_user": None,
+                       "proxy_password": None,
+                       "use_element_name" : normalizeBoolean(use_element_name),
+                       "page_limit" : int(page_limit),
+                       "depth_limit" : int(depth_limit),
+                       "url_filter" : url_filter,
+                       "include_raw_content" : normalizeBoolean(include_raw_content),
+                       "text_separator" : text_separator
+                       }
         
         SearchCommand.__init__(self, run_in_preview=True, logger_name="web_scrape")
         
@@ -30,9 +40,7 @@ class WebScraper(SearchCommand):
         # FYI: we ignore results since this is a generating command
         
         # Do the scraping
-        results = WebInput.scrape_page(self.url, self.selector, self.username, self.password, self.timeout, self.name_attributes, self.output_matches_as_mv, self.output_matches_as_separate_fields, include_empty_matches=False, proxy_type="http", proxy_server=None, proxy_port=None, proxy_user=None, proxy_password=None, use_element_name=self.use_element_name, page_limit=self.page_limit, depth_limit=self.depth_limit, url_filter=self.url_filter)
-        
-        #self.logger.debug("Retrieved results, result=%r", result)
+        results = WebInput.scrape_page(**self.params)
         
         # Output the results
         self.output_results(results)
